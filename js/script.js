865c71efee7a565fbe5bc3d46cf4d41bb17a73ef
Juwel-771 <juwelah99@gmail.com>
2024-07-09 23:40:01 +0600

    phone API created and diplayed to UI

--- js/script.js
+++ js/script.js
@@ -10,15 +10,15 @@ const display = (phones) => {
     const phoneContainer = document.getElementById('phone-container');
     // console.log(phones)
     phones.forEach(phone => {
-        // console.log(phone);
+        console.log(phone);
         const phoneCard = document.createElement("div");
         phoneCard.classList = `card bg-base-100 w-96 shadow-xl`;
         phoneCard.innerHTML = `
         <figure>
-            <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
+            <img src="${phone.image}" alt="Shoes" />
         </figure>
         <div class="card-body">
-            <h2 class="card-title">Shoes!</h2>
+            <h2 class="card-title">${phone.phone_name}</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div class="card-actions justify-end">
                 <button class="btn btn-primary">Buy Now</button>
